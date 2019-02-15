624935495dda242d13f82a83cfd1b6a323133a0c
VelazquezGerardo <46966565+VelazquezGerardo@users.noreply.github.com>
2019-02-14 19:45:45 -0600

    StoryBoard

---
+++ GerardoVelazquez14022019StoryBoard/GerardoVelazquez14022019StoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GerardoVelazquez14022019StoryBoard
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 2/14/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
